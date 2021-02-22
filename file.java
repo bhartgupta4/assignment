e2d8b778b3aa5c9fa2e52c4016f791d11ae434b6
bhartgupta4 <65500557+bhartgupta4@users.noreply.github.com>
2021-02-22 15:03:06 +0530

    Add files via upload

---
+++ file.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+class Student{
+	int id;
+	String name;
+	String year;
+	String cid;
+	String skills;
+	}
+
+class College{
+	int id, nos;
+	String name;
+	String year;
+	String city;
+	String state;
+	String country;
+	List<String> courses;
+}
+
+
+class Main{
+	public static void main(){
+	;
+	}
+}
